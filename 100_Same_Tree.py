101500cc969afca54836b3e3044880e92cac99cc
Allen Huang <allen2990167@gmail.com>
2025-08-28 09:41:40 -0700

    3 - Add DFS method

--- 100_Same_Tree.py
+++ 100_Same_Tree.py
@@ -10,26 +10,37 @@ from typing import Optional
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # DFS
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+        
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
         # BFS
-        queue = deque([(p, q)])
+        # queue = deque([(p, q)])
         
-        while queue:
-            root_p, root_q = queue.popleft()
+        # while queue:
+        #     root_p, root_q = queue.popleft()
             
-            if not root_p and not root_q:
-                continue
+        #     if not root_p and not root_q:
+        #         continue
                 
-            if not root_p or not root_q:
-                return False
+        #     if not root_p or not root_q:
+        #         return False
             
-            if root_p.val != root_q.val:
-                return False
+        #     if root_p.val != root_q.val:
+        #         return False
             
-            queue.append((root_p.left, root_q.left))
-            queue.append((root_p.right, root_q.right))
+        #     queue.append((root_p.left, root_q.left))
+        #     queue.append((root_p.right, root_q.right))
         
-        return True
+        # return True
     
 if __name__ == "__main__":
     sol = Solution()
